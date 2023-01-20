1764ef23da5ad2d5dddc786da149bdb86b162426
garrip91 <garrip91@gmail.com>
2023-01-20 16:38:54 +0300

    Logging Out Users

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register_user, login_page
+from .views import register_user, login_page, logout_user
 
 
 urlpatterns = [
     path("signup/", register_user, name="signup_page"),
     path("login/", login_page, name="login_page"),
+    path("logout/", logout_user, name="logout"),
 ]
