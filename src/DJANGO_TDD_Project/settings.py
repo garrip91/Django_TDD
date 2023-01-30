03832b76db53790c9e0846d51688b9ead1d63e4c
garrip91 <garrip91@gmail.com>
2023-01-30 13:30:35 +0300

    Dockerfile and docker-compose.yml

--- src/DJANGO_TDD_Project/settings.py
+++ src/DJANGO_TDD_Project/settings.py
@@ -45,7 +45,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
+    "django.contrib.admin", # ДЛЯ МИГРАЦИИ ИЗ SQLite3 в PostgreSQL:
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -96,23 +96,33 @@ WSGI_APPLICATION = "DJANGO_TDD_Project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
+""" DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "DjangoTDDDatabase",
+        #"USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432"
+    }
+} """
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("NAME"),
+        #"USER": env("USER"),
+        "USER": "postgres",
+        "PASSWORD": env("PASSWORD"),
+        "HOST": env("HOST"),
+        "PORT": env("PORT")
     }
 }
-# DATABASES = {
-# 'default': {
-# 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-# 'NAME': 'KinomonsterDatabase',
-# 'USER': 'admin',
-# 'PASSWORD': 'admin',
-# #'HOST': 'localhost',
-# 'HOST': '213.139.209.173',
-# 'PORT': '5432'
-# }
-# }
 
 # EMAIL SETTINGS:
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
