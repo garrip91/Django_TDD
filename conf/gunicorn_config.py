b00ef760716fd1e14129c1452f7afaa03b482ef0
garrip91 <garrip91@gmail.com>
2023-01-30 17:06:03 +0300

    any changes

---
+++ conf/gunicorn_config.py
@@ -0,0 +1,4 @@
+command = "/home/garrip91/Programming/DjangoProjects/Django_TDD/venv/bin/gunicorn"
+pythonpath = "/home/garrip91/Programming/DjangoProjects/Django_TDD/src"
+bind = "127.0.0.1:8000"
+#workers = 3
