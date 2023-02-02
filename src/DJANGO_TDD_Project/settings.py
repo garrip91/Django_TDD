595b5172a5619a683f9ef88610811a68477bc8b4
garrip91 <garrip91@gmail.com>
2023-02-02 21:23:45 +0300

    Any modifies in settings.py

--- src/DJANGO_TDD_Project/settings.py
+++ src/DJANGO_TDD_Project/settings.py
@@ -116,8 +116,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": env("NAME"),
-        #"USER": env("USER"),
-        "USER": "postgres",
+        "USER": env("USER"),
         "PASSWORD": env("PASSWORD"),
         "HOST": env("HOST"),
         "PORT": env("PORT")
