ebeaa9db349fabaf7fc1945e14aac76439281097
garrip91 <garrip91@gmail.com>
2023-02-02 21:51:00 +0300

    collectstatic

--- conf/gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-command = "/home/garrip91/Programming/DjangoProjects/Django_TDD/venv/bin/gunicorn"
-pythonpath = "/home/garrip91/Programming/DjangoProjects/Django_TDD/src"
-bind = "127.0.0.1:8000"
-#workers = 3
